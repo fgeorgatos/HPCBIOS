c84fc22b5c2c0ec130808d121c844b0d48c1e564
Fotis Georgatos <fotis.georgatos@uni.lu>
2012-11-14 11:55:33 +0100

    promoted version to v0.3
    Signed-off-by: Fotis Georgatos <fotis.georgatos@uni.lu>

--- testing/docs/conf.py
+++ testing/docs/conf.py
@@ -42,9 +42,9 @@ copyright = '2012, Fotis Georgatos, University of Luxembourg, CC-BY-SA'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.2rc'
+release = '0.3' # 0.3rc1
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
